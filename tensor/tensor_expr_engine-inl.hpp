2c6089cf209b4bf5dfc18a23c7345ed41add1cee
tqchen <tianqi.tchen@gmail.com>
2014-02-12 14:56:43 -0800

    add engine

---
+++ tensor/tensor_expr_engine-inl.hpp
@@ -0,0 +1,17 @@
+#ifndef CXXNET_TENSOR_EXPR_ENGINE_INL_HPP
+#define CXXNET_TENSOR_EXPR_ENGINE_INL_HPP
+/*!
+ * \file tensor_expr_engine-inl.hpp
+ * \brief definitions of how expressions should be evaluated
+ * \author Tianqi Chen
+ */
+#include "tensor_expr.h"
+#include "tensor_algebra.h"
+
+namespace cxxnet{
+    namespace expr{
+        
+    };
+};
+#endif
+
