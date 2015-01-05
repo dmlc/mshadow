c0201ccf3cbb5fa51400c6bcdc10fd3844c9c872
tqchen <tianqi.tchen@gmail.com>
2015-01-04 19:12:25 -0800

    add in stream gpu

---
+++ mshadow/stream_gpu-inl.h
@@ -0,0 +1,62 @@
+/*!
+ *  Copyright (c) 2014 by Contributors
+ * \file stream_gpu-inl.h
+ * \brief implementation of GPU code
+ * \author Bing Xu, Tianqi Chen
+ */
+#ifndef MSHADOW_STREAM_GPU_INL_H_
+#define MSHADOW_STREAM_GPU_INL_H_
+#include "./base.h"
+#include "./tensor.h"
+#include "./utils.h"
+
+namespace mshadow {
+// Stream alocation
+// actual implementation of GPU stream in CUDA
+template<>
+struct Stream<gpu> {
+  /*! \brief cudaStream */
+  cudaStream_t stream_;
+  /*!
+   * \brief wait for all the computation associated
+   *  with this stream to complete
+   */
+  inline void Wait(void) {
+    cudaError_t err = cudaStreamSynchronize(stream_);
+    utils::Check(err == cudaSuccess, cudaGetErrorString(err));
+  }
+  /*!
+   * \brief query whether the the stream is idle
+   * \return true if the stream is idle and all the job have been completed
+   */  
+  inline bool CheckIdle(void) {
+    cudaError_t err = cudaStreamQuery(stream_);
+    if (err == cudaSuccess) return true;
+    if (err == cudaErrorNotReady) return false;
+    utils::Error(cudaGetErrorString(err));
+    return false;
+  }
+  /*!
+   * \brief returns actual cudaStream_t given an input GPU stream pointer
+   * \param stream pointer to GPU stream
+   */
+  inline static cudaStream_t GetStream(Stream<gpu> *stream) {
+    if (stream == NULL) return 0;
+    else return stream->stream_;
+  }
+};
+template<>
+inline Stream<gpu> *NewStream<gpu>(void) {
+  Stream<gpu> *st = new Stream<gpu>();
+  cudaError_t err = cudaStreamCreate(&st->stream_);
+  utils::Check(err == cudaSuccess, cudaGetErrorString(err));
+  return st;
+}
+template<>
+inline void DeleteStream<gpu>(Stream<gpu> *stream) {
+  cudaError_t err = cudaStreamDestroy(stream->stream_);
+  utils::Check(err == cudaSuccess, cudaGetErrorString(err));
+  delete stream;
+}
+}
+#endif  // MSHADOW_STREAM_GPU_INL_H_
