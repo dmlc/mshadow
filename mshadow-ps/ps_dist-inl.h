e68e816ea997f30aaac8ea6a8fa5450acbea9848
muli <muli@cs.cmu.edu>
2015-01-17 16:34:48 -0500

    update ps_dist-inl.h

--- mshadow-ps/ps_dist-inl.h
+++ mshadow-ps/ps_dist-inl.h
@@ -23,15 +23,15 @@ class DistServer : public LocalServer<xpu, DType> {
     Parent::SetParam(name, val);
     if (!strcmp(name, "name")) name_ = val;
     if (!strcmp(name, "parent_name")) parent_name_ = val;
-  }  
+  }
   // initialize the parameter server
   virtual void Init(const std::vector<int> &devices) {
     Parent::Init(devices);
     CHECK(!name_.empty());
     CHECK(!parent_name_.empty());
-    shared_model_ = new PS::KVArray<DType>(name_, parent_name_);    
+    shared_model_ = new PS::KVArray<DType>(name_, parent_name_);
   }
-  virtual ~DistServer(void) {    
+  virtual ~DistServer(void) {
   }
   // override this function, to use parameter server
   virtual void HandlePushFinish(Tensor<cpu, 3, DType> data,
@@ -40,47 +40,38 @@ class DistServer : public LocalServer<xpu, DType> {
     for (index_t i = 1; i < data.size(0); ++i) {
       data[0] += data[i];
     }
-    // things to send and recv
+
+    // push
     Tensor<cpu, 2> sendrecv = data[0];
     using namespace PS;
-    utils::Assert(data[0].CheckContiguous(),
-                  "data must be contiguous");
+    utils::Assert(data[0].CheckContiguous(), "data must be contiguous");
     // TODO the zero copy version
     // SArray<DType> val(data.dptr_, data.MSize(), false);
     SArray<DType> val; val.copyFrom(sendrecv.dptr_, sendrecv.MSize());
-    MessagePtr msg(new Message(kServerGroup));
-    msg->addValue(val);
-    msg->task.set_key_channel(key);
-    Range<Key>(0, val.size()).to(msg->task.mutable_key_range());
-    records_[key].push = CHECK_NOTNULL(shared_model_)->push(msg);
-    // setup callback
-    auto& rec = records_[key];
-    MessagePtr msg(new Message(kServerGroup, -1, rec.push));
-    msg->task.set_key_channel(key);
-    Range<Key>(0, sendrecv.MSize()).to(msg->task.mutable_key_range());
-    
-    msg->fin_handle = [this, sendrecv, key]() {
-      const auto& recv = shared_model_->array(key);
-      CHECK_EQ(sendrecv.MSize(), recv.size());      
-      memcpy(CHECK_NOTNULL(sendrecv.dptr_), recv.data(), recv.size() * sizeof(DType));
+    MessagePtr push_msg(new Message(kServerGroup));
+    push_msg->addValue(val);
+    push_msg->task.set_key_channel(key);
+    Range<Key>(0, val.size()).to(push_msg->task.mutable_key_range());
+    int push_time = CHECK_NOTNULL(shared_model_)->push(push_msg);
+
+    // pull
+    MessagePtr pull_msg(new Message(kServerGroup, -1, push_time));
+    pull_msg->task.set_key_channel(key);
+    Range<Key>(0, sendrecv.MSize()).to(pull_msg->task.mutable_key_range());
+    shared_model_->setArray(key, sendrecv.dptr_, sendrecv.MSize());
+    pull_msg->fin_handle = [this, sendrecv, key]() {
       // call PullReady to notify LocalServer pulling is ready
-      this->PullReady(sendrecv, key);      
+      this->PullReady(sendrecv, key);
     };
-    rec.pull = CHECK_NOTNULL(shared_model_)->pull(msg);    
+    shared_model_->pull(pull_msg);
   }
 
  private:
-  struct Record {
-    int push = -1;
-    int pull = -1;
-    DType* data = nullptr;
-  };
   std::string name_;
   std::string parent_name_;
-  PS::KVArray<DType>* shared_model_;
+  PS::KVArray<DType>* shared_model_ = nullptr;
 };
 #endif
 }  // namespace ps
 }  // namespace msahdow
 #endif
-
