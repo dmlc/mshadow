3053f8cdfea0274739282ced015ad458090760e8
Bing Xu <antinucleon@gmail.com>
2015-09-06 23:31:21 -0600

    fix softmax

--- mshadow/cuda/tensor_gpu-inl.cuh
+++ mshadow/cuda/tensor_gpu-inl.cuh
@@ -190,12 +190,12 @@ template<int x_bits, typename DType, typename DstPlan, typename SrcPlan1, typena
 __global__ void SoftmaxGradKernel(DstPlan dst, SrcPlan1 src, SrcPlan2 label, index_t xmax) {
   const int y = blockIdx.x;
   const int x = threadIdx.x;
-  const int k = static_cast<int>(label.Eval(y, 0));
+  const int k = static_cast<int>(label.Eval(0, y));
   if (x < xmax) {
     if (x == k) {
       dst.REval(y, k) = src.Eval(y, k) - 1.0f;
     } else {
-      dst.REval(y, k) = src.Eval(y, k);
+      dst.REval(y, x) = src.Eval(y, x);
     }
   }
 }
