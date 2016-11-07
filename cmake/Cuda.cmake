c99d18a26615390a325db43931b0b93ccb734e8c
Alex Newman <anewman@wolfram.com>
2016-11-07 13:30:55 -0500

    Search the standard include and lib for cuDNN
    CMake will not do any further processing of the NAMES under find_library
    and find_path, so they should be explicit paths beneath CUDNN_ROOT.
    Also, CUDNN_INCLUDE has been moved to the header search given that the
    library would not be expected to be located beside the header under
    normal circumstances. In an irregular distribution, CUDNN_ROOT would suffice to
    find both.

--- cmake/Cuda.cmake
+++ cmake/Cuda.cmake
@@ -195,12 +195,12 @@ function(detect_cuDNN)
   set(CUDNN_ROOT "" CACHE PATH "CUDNN root folder")
 
   find_path(CUDNN_INCLUDE cudnn.h
-            PATHS ${CUDNN_ROOT} $ENV{CUDNN_ROOT} ${CUDA_TOOLKIT_INCLUDE}
+            PATHS ${CUDNN_ROOT} $ENV{CUDNN_ROOT} ${CUDNN_ROOT}/include ${CUDNN_INCLUDE} ${CUDA_TOOLKIT_INCLUDE}
             DOC "Path to cuDNN include directory." )
 
   get_filename_component(__libpath_hist ${CUDA_CUDART_LIBRARY} PATH)
   find_library(CUDNN_LIBRARY NAMES cudnn # libcudnn_static.a
-                             PATHS ${CUDNN_ROOT} $ENV{CUDNN_ROOT} ${CUDNN_INCLUDE} ${__libpath_hist}
+                             PATHS ${CUDNN_ROOT} $ENV{CUDNN_ROOT} ${CUDNN_ROOT}/lib64 ${CUDNN_ROOT}/lib ${__libpath_hist}
                              DOC "Path to cuDNN library.")
 
   if(CUDNN_INCLUDE AND CUDNN_LIBRARY)
