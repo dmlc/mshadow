47b579a63a234126f98c0fa5c2f2df91a0408b90
Eric Junyuan Xie <piiswrong@users.noreply.github.com>
2017-01-20 19:36:55 -0800

    Update tensor_cpu-inl.h

--- mshadow/tensor_cpu-inl.h
+++ mshadow/tensor_cpu-inl.h
@@ -151,7 +151,9 @@ inline void MapPlan(TRValue<R, cpu, dim, DType> *dst,
                     const expr::Plan<E, DType> &plan) {
   Shape<2> shape = expr::ShapeCheck<dim, R>::Check(dst->self()).FlatTo2D();
   expr::Plan<R, DType> dplan = expr::MakePlan(dst->self());
+#if (MSHADOW_USE_CUDA == 0)
   #pragma omp parallel for
+#endif
   // temp remove openmp, as default setting throttles CPU
   for (index_t y = 0; y < shape[0]; ++y) {
     for (index_t x = 0; x < shape[1]; ++x) {
@@ -218,7 +220,9 @@ inline void MapReduceKeepLowest(TRValue<R, cpu, 1, DType> *dst,
   // execution
   expr::Plan<R, DType> dplan = MakePlan(dst->self());
   expr::Plan<E, DType> splan = MakePlan(exp.self());
+#if (MSHADOW_USE_CUDA == 0)
   #pragma omp parallel for
+#endif
   for (index_t x = 0; x < eshape[1]; ++x) {
     DType res = splan.Eval(0, x);
     for (index_t y = 1; y < eshape[0]; ++y) {
@@ -249,7 +253,9 @@ inline void MapReduceKeepHighDim(TRValue<R, cpu, 1, DType> *dst,
   // execution
   expr::Plan<R, DType> dplan = MakePlan(dst->self());
   expr::Plan<E, DType> splan = MakePlan(exp.self());
+#if (MSHADOW_USE_CUDA == 0)
   #pragma omp parallel for
+#endif
   for (index_t c = 0; c < pshape[1]; ++c) {
     DType res; Reducer::SetInitValue(res);
     for (index_t n = 0; n < pshape[0]; ++n) {
