8b35224e83419b1571cc5a0c58f84223fb80c22a
yajiedesign <yajiedesign@gmail.com>
2018-04-27 10:33:44 +0800

    change to all clang

--- mshadow/base.h
+++ mshadow/base.h
@@ -139,7 +139,8 @@ typedef unsigned __int64 uint64_t;
 #ifndef MSHADOW_USE_F16C
   #if defined(_MSC_VER)
     #define MSHADOW_USE_F16C 0
-  #elif defined(__APPLE__) && ((__clang_major__ < 8) || ((__clang_major__ == 8) && (__clang_minor__ < 1)))
+  #elif defined(__clang__) && 
+        ((__clang_major__ < 8) || ((__clang_major__ == 8) && (__clang_minor__ < 1)))
     #define MSHADOW_USE_F16C 0  
   #else
     #define MSHADOW_USE_F16C 1
